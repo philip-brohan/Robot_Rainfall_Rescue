7cbe72f44cf3d260b33fd1d7cff780deb4a05337
Philip Brohan <philip@brohan.org>
2021-02-23 17:35:54 +0000

    Added convolutional transcription models for the cell training data.

---
+++ models/transcriber_single_cell/perturbed/transcriberModel.py
@@ -0,0 +1,61 @@
+# Model specification for a single cell image transcriber
+
+import tensorflow as tf
+
+# This file provides a subclass of tf.keras.Model that serves as a
+#  transcriber for the ATB2 images. It learns to make a tensor of
+#  extracted digits from a tensor of the document image.
+
+# import this file, instantiate an instance of the transcriberModel
+#  class, and then either train it and save the weights, or load
+#  pre-trained weights and use it for transcription.
+
+# Model the image with hierachical convolutions and then map to output digits
+class transcriberModel(tf.keras.Model):
+    def __init__(self):
+        # parent constructor
+        super(transcriberModel, self).__init__()
+        # Initial shape (64,64,1)
+        self.conv1A = tf.keras.layers.Conv2D(
+            40,
+            (3, 3),
+            strides=(2, 2),
+            padding="valid",
+            kernel_regularizer=tf.keras.regularizers.l2(0.001),
+        )
+        self.act1A = tf.keras.layers.ELU()
+        # Now (32,32,10)
+        self.conv1B = tf.keras.layers.Conv2D(
+            80,
+            (3, 3),
+            strides=(2, 2),
+            padding="valid",
+            kernel_regularizer=tf.keras.regularizers.l2(0.001),
+        )
+        self.act1B = tf.keras.layers.ELU()
+        # Now (16,16,20)
+        # reshape to 1d
+        self.flatten = tf.keras.layers.Flatten()
+        # map directly to output format (3 digits)
+        self.map_to_op = tf.keras.layers.Dense(
+            3 * 10, kernel_regularizer=tf.keras.regularizers.l1(0.0001)
+        )
+        # softmax to get digit probabilities at each location
+        self.op_reshape = tf.keras.layers.Reshape(
+            target_shape=(
+                3,
+                10,
+            )
+        )
+        self.op_softmax = tf.keras.layers.Softmax(axis=2)
+
+    def call(self, inputs):
+        x = self.conv1A(inputs)
+        x = self.act1A(x)
+        x = self.conv1B(x)
+        x = self.act1B(x)
+        x = self.flatten(x)
+        x = self.map_to_op(x)
+        x = self.op_reshape(x)
+        x = self.op_softmax(x)
+        return x
---
+++ training_data/single_cell/make_set_of_images_perturbed.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+
+# Make 10,000 fake cell images, each with associated data file
+
+import os
+import random
+
+# Get the list of fonts to use
+from fonts import fontNames
+from fonts import fontScales
+
+image_dir = "%s/Robot_Rainfall_Rescue/training_data/cell/perturbed/" % os.getenv(
+    "SCRATCH"
+)
+
+f = open("run_mi.sh", "w+")
+
+for idx in range(10000):
+    fn = "%s/images/%04d.png" % (image_dir, idx)
+    if os.path.exists(fn):
+        continue
+    fontFamily = "Arial"
+    fontStyle = "normal"
+    fontWeight = "normal"
+    fontSize = 12
+    if fontFamily in fontScales:
+        fontSize *= fontScales[fontFamily]
+    xshift = random.randint(-5, 5)
+    yshift = random.randint(-5, 5)
+    xscale = random.normalvariate(1, 0.03)
+    yscale = random.normalvariate(1, 0.03)
+    rotate = random.normalvariate(0, 3)
+    jitterFontRotate = random.normalvariate(0, 3)
+    jitterFontSize = random.normalvariate(0, 1)
+    jitterGridPoints = random.normalvariate(0, 0.001)
+    jitterLineWidth = random.normalvariate(0, 0.25)
+    f.write(
+        (
+            './make_image_data_pair.py --opdir=%s --docn="%04d"'
+            + " --xshift=%d --yshift=%d --xscale=%f --yscale=%f"
+            + " --rotate=%f --jitterFontRotate=%f --jitterFontSize=%f"
+            + " --jitterGridPoints=%f --jitterLineWidth=%f"
+            + " --fontFamily='%s' --fontSize=%f"
+            + " --fontStyle=%s --fontWeight=%s\n"
+        )
+        % (
+            image_dir,
+            idx,
+            xshift,
+            yshift,
+            xscale,
+            yscale,
+            rotate,
+            jitterFontRotate,
+            jitterFontSize,
+            jitterGridPoints,
+            jitterLineWidth,
+            fontFamily,
+            fontSize,
+            fontStyle,
+            fontWeight,
+        )
+    )
